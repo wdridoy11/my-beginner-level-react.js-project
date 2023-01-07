9dc1a6f8dc06353d800b376f042529437c8fdbd4
Ridoy Sharif <developersridoy@gmail.com>
2023-01-07 11:14:43 +0600

    created react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import style from './app.css';
+const app = () => {
+   return (
+      <div>
+         <h1 className='headeringStyle'>Todo App</h1>
+         <h3>Call Family</h3>
+         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, odit?</p>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+   )
 }
 
-export default App;
+export default app
