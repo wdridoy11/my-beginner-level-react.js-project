36fa8e0717687093ba4706d891092378e3e16d79
Ridoy Sharif <developersridoy@gmail.com>
2023-01-07 12:12:06 +0600

    created card send data props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import style from './app.css';
+import Card from './card'
+import Data from './data.json';
+
+const title = "TODO App";
 const app = () => {
    return (
       <div>
-         <h1 className='headeringStyle'>Todo App</h1>
-         <h3>Call Family</h3>
-         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat, odit?</p>
-
+         <h1>{title}</h1>
+         <Card title={Data[0].title} description={Data[0].description} />
+         <Card title={Data[1].title} description={Data[1].description} />
+         <Card title={Data[2].title} description={Data[2].description} />
+         <Card title={Data[3].title} description={Data[3].description} />
+         <Card title={Data[4].title} description={Data[4].description} />
+         <Card title={Data[5].title} description={Data[5].description} />
       </div>
    )
 }
