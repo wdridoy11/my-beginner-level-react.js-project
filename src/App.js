9ad69899b50129a1e00d6228bae224bf5e25314d
Ridoy Sharif <developersridoy@gmail.com>
2023-01-07 16:35:00 +0600

    Nester maping info item

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid';
 import Card from './card'
 import Data from './data.json';
+import style from './app.css'
+const info = [
+   {
+      firstName: "Riody",
+      lastName: "Sharif",
+      age: "16",
+      phone: [
+         { home: "01722824160" },
+         { office: "01722540581" }
+      ],
+      address: [
+         { city: "Rangpur" },
+         { village: "ShikerPur" }
+      ],
+      study: "Ten"
+   }
+]
 
-const title = "TODO App";
 const app = () => {
    return (
       <div>
-         <h1>{title}</h1>
-         <Card title={Data[0].title} description={Data[0].description} />
-         <Card title={Data[1].title} description={Data[1].description} />
-         <Card title={Data[2].title} description={Data[2].description} />
-         <Card title={Data[3].title} description={Data[3].description} />
-         <Card title={Data[4].title} description={Data[4].description} />
-         <Card title={Data[5].title} description={Data[5].description} />
+         {info.map((item) => {
+            return (
+               <div>
+                  <h3>{item.firstName}</h3>
+                  <h3>{item.lastName}</h3>
+                  <p>{item.study}</p>
+                  <p>{item.age}</p>
+                  {item.phone.map((item) => <div>
+                     <p>{item.home}</p>
+                     <p>{item.office}</p>
+                  </div>)}
+                  {item.address.map((item) => <div>
+                     <p>{item.city}</p>
+                     <p>{item.village}</p>
+                  </div>)}
+               </div>
+            )
+         })}
       </div>
    )
 }
--- src/card.js
+++ src/card.js
@@ -1,28 +1,14 @@
 import React from 'react'
-import style from './app.css';
 
-
-const subTitle = "Family";
-const description = "Hello Bangladesh! I am very happy";
-const data = new Date();
-const day = data.getDay();
-const month = data.getMonth();
-const year = data.getYear();
-console.log(data);
-
-
-function Card(props) {
-   let { title, description } = props;
+const card = (props) => {
+   let { img, title, description } = props;
    return (
-      <div>
-         <div className='card'>
-            <h3 className='card-title'>{title}</h3>
-            <p className='card-description'>{description}</p>
-            <p className='card-date'>{day}/{month}/{year}</p>
-            <button className='card-btn'>Read More</button>
-         </div>
+      <div className='card'>
+         <img className='card-img' src={img} alt="" />
+         <h3 className='card-title'>{title}</h3>
+         <p className='card-description'>{description}</p>
       </div>
    )
 }
 
-export default Card;
+export default card
